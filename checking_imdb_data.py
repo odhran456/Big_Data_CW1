480d89b62bde17008da565e5e9cdfcdfa2c4fca1
odhran456 <44904580+odhran456@users.noreply.github.com>
2020-02-02 23:54:11 +0000

    Update checking_imdb_data.py

--- checking_imdb_data.py
+++ checking_imdb_data.py
@@ -4,10 +4,7 @@ import pandas as pd
 
 actors_fp = '/Users/Odhran/PycharmProjects/CrashCourse/imdb/actors.csv'
 actors = pd.read_csv(actors_fp, delimiter=';')
-# print(actors.shape, actors.head, actors.dtypes)
 
-# actors = np.genfromtxt(actors_fp, delimiter=';', encoding='utf-8', skip_header=1, dtype=(int,'U25','U25'))
-# gender_problem = np.empty([1,3], dtype=(int,'U25','U25'))
 
 male_actors=0
 female_actors=0
